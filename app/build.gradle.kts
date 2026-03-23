7efe1d1595877a5ce2f6707c1896dbce3a58f422
parip69 <gerhardbachner@gmail.com>
2026-03-23 08:43:18 +0100

    14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "de.parip69.barcodeaudiscanner"
         minSdk = 24
         targetSdk = 35
-        versionCode = 13
-        versionName = "13"
+        versionCode = 14
+        versionName = "14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
