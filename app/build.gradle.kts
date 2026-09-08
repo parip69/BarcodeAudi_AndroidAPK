63fcb485f16d7c11fdbf585c0b78bc11895f3775
parip69 <gerhardbachner@gmail.com>
2026-09-08 15:56:51 +0200

    Ver. 60: Stern/geschweifte Klammer (*/}) Tastatur-Swap fuer EN-Layout ergaenzt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "de.parip69.barcodeaudiscanner"
         minSdk = 24
         targetSdk = 35
-        versionCode = 59
-        versionName = "59"
+        versionCode = 60
+        versionName = "60"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
